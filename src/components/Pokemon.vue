a175a16ee8cdad737a3fcd3051590177e3cb411f
Joao-Gontijo <jvgontijo166@gmail.com>
2021-05-20 21:18:11 -0300

    Colocando nomes em letra maiuscula
    Listando o nome dos pokémon na tela e colocando a primeira letra como maiuscula.

---
+++ src/components/Pokemon.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <h1>{{num}} {{name | upper}}</h1>
+        <small>{{url}}</small>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        num: Number,
+        name: String,
+        url: String
+    },
+    filters: {
+        upper: function(value) { //recebendo o texto, separando em duas partes, para colocar a primeira letra como maiuscula.
+            var newName = value[0].toUpperCase() + value.slice(1);
+            return newName;
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
